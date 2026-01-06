b86c36dae3f8fa091093669833ecbf3df0410591
OGEmanuel <1blacksanta416@gmail.com>
2026-01-06 17:17:12 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,41 @@
 import Image from "next/image";
+import LogoIcon from "./components/logo-icon";
+import footer from "@/public/footer.png";
+import footerMobile from "@/public/footer-mobile.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="bg-[#FEFCF9] h-screen flex justify-center items-center">
+      <Image
+        src={footer}
+        alt="footer"
+        className="absolute bottom-0 max-sm:hidden"
+      />
+      <Image
+        src={footerMobile}
+        alt="footer"
+        className="absolute bottom-0 sm:hidden _w-full"
+      />
+      <div className="flex items-center flex-col max-w-88.25 sm:max-w-149 z-10 w-full text-center gap-8">
+        <LogoIcon size="56" className="max-sm:hidden" />
+        <LogoIcon size="48" className="sm:hidden" />
+        <div className="flex flex-col gap-6 items-center">
+          <h1 className="text-[#212121] text-[2rem]/10 sm:text-[56px]/16 font-sans-serif">
+            Guest lists & invitations, made effortless
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="text-[#575554] leading-6 sm:text-xl/7 -tracking-[0.02em] font-inter">
+            Collect guest details, send beautiful invitations, and track
+            responses — all in one calm, organized place.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Link
+            href={"https://invia-rosy.vercel.app/auth/signin"}
+            className="text-white bg-[#479FFD] cursor-pointer w-max text-xl/7 active:scale-95 transition-transform -tracking-[0.02em] py-3 sm:py-4 px-5 sm:px-6 rounded-[99px] font-medium"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Try for free!
+          </Link>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
